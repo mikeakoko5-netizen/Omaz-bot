59b2fb46fda6626d3bd89b722c0aa8303ab31088
mikeakoko5-netizen <mikeakoko5@gmail.com>
2026-06-13 09:29:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+   from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import json
@@ -174,3 +174,4 @@ def telegram():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+     
